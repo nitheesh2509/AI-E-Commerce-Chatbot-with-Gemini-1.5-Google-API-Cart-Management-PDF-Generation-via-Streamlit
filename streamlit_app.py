e192b6d569fa39b8e766a35704828767d7b40387
Nitheeshkumar <lovelynitheesh123@gmail.com>
2025-01-07 17:38:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from fpdf import FPDF
+from fpdf2 import FPDF
 
 import google.generativeai as genai  # Make sure you have the Gemini API installed and set up
 
